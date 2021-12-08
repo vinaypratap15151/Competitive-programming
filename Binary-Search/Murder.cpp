572e54e4e430b2282562d2217fe9dc17bc8e031f
vinay pratap singh rana <75728781+vinaypratap15151@users.noreply.github.com>
2021-12-09 02:09:41 +0530

    Update Murder.cpp

--- Binary-Search/Murder.cpp
+++ Binary-Search/Murder.cpp
@@ -1,5 +1,8 @@
 // Once detective Saikat was solving a murder case. While going to the crime scene he took the stairs and saw that a number is written on every stair.
-//  He found it suspicious and decides to remember all the numbers that he has seen till now. While remembering the numbers he found that he can find some pattern in those numbers. So he decides that for each number on the stairs he will note down the sum of all the numbers previously seen on the stairs which are smaller than the present number. Calculate the sum of all the numbers written on his notes diary.
+//  He found it suspicious and decides to remember all the numbers that he has seen till now. While remembering the numbers he found that
+// he can find some pattern in those numbers.
+// So he decides that for each number on the stairs he will note down the sum of all the numbers previously seen on the stairs which are smaller than the present number.
+//  Calculate the sum of all the numbers written on his notes diary.
 // Answer may not fit in integer . You have to take long.
      
 // Input Format:
